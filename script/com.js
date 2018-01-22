9f60a864e4e5d81435594facf981a90598dfd9b3
wuhaodi93 <whdjlyx@163.com>
2018-01-22 09:51:33 +0800

    复制完成

---
+++ script/com.js
@@ -0,0 +1,18 @@
+
+
+function cc() {
+  var config={
+    "getLunboUrl":"123",
+    "getcc":"456"
+  }
+  return config;
+}
+
+function common2() {
+     var common3={};
+
+     common3.tt=function(url) {
+       alert(url+"...url");
+     }
+    return common3;
+}
